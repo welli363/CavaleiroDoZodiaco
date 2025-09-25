336ce39215492de400c200e3e98d34224c7dcb88
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2025-09-25 09:16:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#292929',
     alignItems: 'center',
     justifyContent: 'center',
   },
