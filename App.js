809fb92cff1a1617e831dbf15758534c25127a3e
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2025-09-25 09:14:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 60 }}>Deficiência Visual:</Text>
+      <Text style={{ fontSize: 30, color: 'white' }}>Deficiência Visual:</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
