88e64f48395b322dbda75e368dbfcc3df8e259c5
welli363 <toncosta08@gmail.com>
2025-09-25 11:11:02 -0300

    commit subtitulo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Titulo from "./src/components/Titulo"
+import Subtitulo from './src/components/Subtitulo';
 
 export default function App() {
   return (
     <View style={styles.container}>
     <Titulo/>
+    <Subtitulo/>
     </View>
   );
 }
---
+++ src/components/Subtitulo.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Subtitulo (){
+    return (
+        <Text style={{fontSize: 20, color: '#fff'}}>
+            Deficiência Visual
+        </Text>
+    )
+}
