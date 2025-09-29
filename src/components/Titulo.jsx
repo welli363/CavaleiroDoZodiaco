173fcbd222452556e7d5993f96e4f24fa33aeb1f
Wellington da Costa <toncosta08@gmail.com>
2025-09-29 18:53:04 -0300

    d
    dededd

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -4,6 +4,6 @@ export default function Titulo() {
   return (
     <Text style={{fontSize: 30, color: '#fff', alignItems: 'center', marginTop: 40, fontWeight: 'bold'}}>
         Semana da Inclusão{"\n"} a Pessoa Deficiente
-    </Text>
+    </Text >
   );
 }
