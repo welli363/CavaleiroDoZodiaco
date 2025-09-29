211b18db56ec014325522fb61ca8f32576fcb077
Wellington da Costa <toncosta08@gmail.com>
2025-09-29 19:17:50 -0300

    troquei a cor do titulo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Titulo from "./src/components/Titulo"
 import Subtitulo from './src/components/Subtitulo';
 import Texto from "./src/components/Texto"
@@ -7,12 +7,14 @@ import TextoCausas from "./src/components/TextoCausas"
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
     <Titulo/>
     <Subtitulo/>
     <Texto/>
     <TextoCausas/>
     </View>
+    </ScrollView>
   );
 }
 
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Titulo() {
   return (
-    <Text style={{fontSize: 30, color: '#fff', alignItems: 'center', marginTop: 40, fontWeight: 'bold'}}>
+    <Text style={{fontSize: 30, color:"gold", alignItems: 'center', marginTop: 40, fontWeight: 'bold'}}>
         Semana da Inclusão{"\n"} a Pessoa Deficiente
     </Text >
   );
