5ee61ecdb196dbb858b0b260eceb2614992c1a0a
welli363 <toncosta08@gmail.com>
2025-09-30 10:45:22 -0300

    arrumei os códigos da imagem

--- src/components/Imagem.jsx
+++ src/components/Imagem.jsx
@@ -1,13 +1,20 @@
 
 import { StyleSheet, Text, View, Image } from 'react-native';
+import imagem from '../../assets/defvisu.jpg'
 
 export default function Imagem (){
     return(
         <View style={styles.container}>
-            <Imagem  source={require('../assets/defvisu.jpg')} 
-                style={styles.imagem}
-            >
-            </Imagem>
+            <Image  source={imagem} style={styles.imagem} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+    imagem: {
+
+    }
+})
